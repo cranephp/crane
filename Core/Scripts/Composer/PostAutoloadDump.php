9cb79e726e9380cc2c7c8d6a62a512e298fb8084
Peter2962 <mayokundamilare@gmail.com>
2019-03-09 09:20:45 +0200

    Build update - Updated composer command scripts, .htaccess and .gitignore

---
+++ Core/Scripts/Composer/PostAutoloadDump.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @package 	Core.Scripts.Composer.PostAutoloadDump
+* @author 	Peter Taiwo
+*/
+
+namespace Core\Scripts\Composer;
+
+Class PostAutoloadDump
+{
+
+	/**
+	* Runs when composer command 'composer dump-autoload' is run.
+	*
+	* @access 	public
+	* @static
+	* @return 	void
+	*/
+	public static function runScript()
+	{
+
+	}
+
+}
---
+++ Core/Scripts/Composer/PostUpdateCommand.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @package 	Core.Scripts.Composer.PostUpdateDump
+* @author 	Peter Taiwo
+*/
+
+namespace Core\Scripts\Composer;
+
+Class PostUpdateDump
+{
+
+	/**
+	* Runs when composer command 'composer install' is run.
+	*
+	* @access 	public
+	* @static
+	* @return 	void
+	*/
+	public static function runScript()
+	{
+
+	}
+
+}
