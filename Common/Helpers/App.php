c7d09b8e4ba43189f88c6dbedaef3345e4ee5993
Peter2962 <mayokundamilare@gmail.com>
2019-03-10 06:29:57 +0200

    Build update - Updated Common\Helpers\App helper class

--- Common/Helpers/App.php
+++ Common/Helpers/App.php
@@ -4,5 +4,53 @@ namespace Common\Helpers;
 
 class App
 {
-	
+
+	/**
+	* Returns the project's root directory.
+	*
+	* @access 	public
+	* @static
+	* @return 	String
+	*/
+	public static function getRootDirectory()
+	{
+		return ROOT_DIRECTORY;
+	}
+
+	/**
+	* Returns the project's common directory.
+	*
+	* @access 	public
+	* @static
+	* @return 	String
+	*/
+	public static function getCommonDirectory()
+	{
+		return COMMON_DIRECTORY;
+	}
+
+	/**
+	* Returns the project's public directory.
+	*
+	* @access 	public
+	* @static
+	* @return 	String
+	*/
+	public static function getPublicDirectory()
+	{
+		return PUBLIC_DIRECTORY;
+	}
+
+	/**
+	* Returns the project's core directory.
+	*
+	* @access 	public
+	* @static
+	* @return 	String
+	*/
+	public static function getCoreDirectory()
+	{
+		return CORE_DIRECTORY;
+	}
+
 }
