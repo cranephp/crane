5b5b912c80ec99d025a94e7d0b22cc72fa96be34
Peter2962 <mayokundamilare@gmail.com>
2019-03-10 06:21:22 +0200

    Build update - Updating bootstrap logic

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,1 +1,9 @@
 <?php
+
+if (!file_exists(AUTOLOAD_FILE)) {
+	throw new RuntimeException(
+		sprintf('autoload file bould not be loaded in [%s]', AUTOLOAD_FILE)
+	);
+}
+
+require_once(AUTOLOAD_FILE);
